75d32fdcf853bcc4648b70590f2b26f480ff940b
Giulia Bonaspetti <giulia.bonaspetti@algoo.fr>
2022-10-21 15:22:50 +0200

    handle received messages on webview

--- src/Webview.jsx
+++ src/Webview.jsx
@@ -6,38 +6,45 @@ import {
 import {
   ActivityIndicator,
   BackHandler,
+  Platform,
   RefreshControl,
   ScrollView,
   View
 } from 'react-native'
 import { WebView } from 'react-native-webview'
+import { getCredentials, removeCredentials } from './authentificationHelper.js'
 import { styles } from './styles.js'
 
 export const Webview = (props) => {
   const [refresherEnabled, setEnableRefresher] = useState(false)
   const [canGoBack, setCanGoBack] = useState(false)
-  const [url, setUrl] = useState('')
+  const [httpsUrl, setHttpsUrl] = useState('')
+  const [screenId, setScreenId] = useState(props.screenId)
   const [isRefLoaded, setIsRefLoaded] = useState(false)
   const webViewRef = useRef()
 
   useEffect(() => {
-    if (props.screenId) setUrl(`https://${props.url}/ui/${props.screenId}`)
-    else setUrl(`https://${props.url}`)
-
     setIsRefLoaded(true)
   }, [])
 
   useEffect(() => {
-    const goBackAction = () => {
-      // FIXME - G.B. - 2022-10-03 - Need to include canGoBack here
-      if (webViewRef.current) {
-        webViewRef.current.goBack()
-      } else props.onClickGoBack()
-      return true
-    }
-    BackHandler.addEventListener('hardwareBackPress', goBackAction)
+    if (screenId) setHttpsUrl(`https://${props.url}/ui/${screenId}`)
+    else setHttpsUrl(`https://${props.url}`)
+  }, [screenId])
 
-    return () => BackHandler.removeEventListener('hardwareBackPress', goBackAction)
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      const goBackAction = () => {
+        // FIXME - G.B. - 2022-10-03 - Need to include canGoBack here
+        if (webViewRef.current) {
+          webViewRef.current.goBack()
+        } else props.onClickGoBack()
+        return true
+      }
+      BackHandler.addEventListener('hardwareBackPress', goBackAction)
+
+      return () => BackHandler.removeEventListener('hardwareBackPress', goBackAction)
+    }
   }, [canGoBack, isRefLoaded])
 
   const handleNavigationStateChange = (e) => setCanGoBack(e.canGoBack)
@@ -46,7 +53,19 @@ export const Webview = (props) => {
     const isTopOfPage = Number(e.nativeEvent.contentOffset.y) === 0
     // FIXME - G.B. - 2022-09-30 - The refresh is disabled until the problem is fixed in Tracim.
     // The mechanism works for the page scroll as a whole, but not for a scroll on a very internal div.
-    //setEnableRefresher(isTopOfPage)
+    // setEnableRefresher(isTopOfPage)
+  }
+
+  const handleMessageReceived = async (e) => {
+    if (e.nativeEvent.data === 'login') {
+      const credentials = await getCredentials(props.url)
+      if (credentials) {
+        setScreenId('recent-activities')
+      }
+    }
+    if (e.nativeEvent.data === 'logout') {
+      removeCredentials(props.url)
+    }
   }
 
   return (
@@ -64,13 +83,14 @@ export const Webview = (props) => {
         onNavigationStateChange={handleNavigationStateChange}
         onScroll={handleScroll}
         ref={webViewRef}
-        source={{ uri: url }}
+        source={{ uri: httpsUrl }}
         startInLoadingState
         renderLoading={() => (
           <View style={styles.pageContainer}>
             <ActivityIndicator size='large' />
           </View>
         )}
+        onMessage={handleMessageReceived}
       />
     </ScrollView>
   )
