7fa6c408d6d02ea044aa0c0c744d9f70ec870c7e
Giulia Bonaspetti <giulia.bonaspetti@algoo.fr>
2022-11-03 10:59:28 +0100

    update camera config ios and splash screen android

--- android/app/src/main/java/com/mobile_app/MainActivity.java
+++ android/app/src/main/java/com/mobile_app/MainActivity.java
@@ -4,14 +4,11 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
-import org.devio.rn.splashscreen.SplashScreen;
-
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
     super.onCreate(savedInstanceState);
   }
 
