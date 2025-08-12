222e95190c348ddb812da4d5127b52f3e50e27b4
Skylsmoi <come.huguies@algoo.fr>
2025-08-12 11:52:27 +0200

    feat(env): fix build setup

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin']
+  plugins: [
+      'react-native-reanimated/plugin',
+      'react-native-worklets/plugin', // INFO - CH - 2025-08-12 - must be last
+  ]
 };
